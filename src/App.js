a2f01a5606fc25a24500353048b204c1ad2a5aec
jerald <jeraldy@hawaii.edu>
2022-02-15 13:57:25 -1000

    hotjar import

--- src/App.js
+++ src/App.js
@@ -53,6 +53,9 @@ import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "co
 import brandWhite from "assets/images/logo-ct.png";
 import brandDark from "assets/images/logo-ct-dark.png";
 
+//hotjar
+import { hotjar } from 'react-hotjar'
+
 export default function App() {
   const [controller, dispatch] = useMaterialUIController();
   const {
