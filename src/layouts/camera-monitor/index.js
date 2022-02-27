70a0362f109c84179382ff712fdc4d102224d6a1
jerald <jeraldy@hawaii.edu>
2022-02-26 23:43:56 -1000

    camera device UI

--- src/layouts/camera-monitor/index.js
+++ src/layouts/camera-monitor/index.js
@@ -23,12 +23,58 @@ import MDBox from "components/MDBox";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+
+// Data
+import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
 
 function CameraMonitor() {
-
+  const { sales, tasks } = reportsLineChartData;
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      <MDBox py={3}>
+        <MDBox mt={4.5}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsBarChart
+                  color="info"
+                  title="Device 1 "
+                  description="Latest Preview"
+                  date="Insight discovered 2 days ago"
+                  chart={reportsBarChartData}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+              <ReportsBarChart
+                  color="info"
+                  title="Device 2"
+                  description="Latest Preview"
+                  date="Insight discovered 2 days ago"
+                  chart={reportsBarChartData}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+              <ReportsBarChart
+                  color="info"
+                  title="Device 3"
+                  description="Latest Preview"
+                  date="Insight discovered 2 days ago"
+                  chart={reportsBarChartData}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
+      </MDBox>
       <Footer />
     </DashboardLayout>
   );
